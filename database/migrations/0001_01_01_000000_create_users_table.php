ba4cfa7b77b3ae43f2ddd7558d3c7eccc14bb093
Aidil <aidilpons04@gmail.com>
2025-11-28 01:51:00 +0700

    fix: update users table migration (replace fullname with name)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fullname');
+            $table->string('name');
             $table->string('nim', 20)->unique();
             $table->string('email')->unique();
             $table->string('faculty')->nullable();
